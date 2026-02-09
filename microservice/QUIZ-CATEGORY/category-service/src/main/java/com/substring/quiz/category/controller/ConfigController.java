fe323308b5cebb6c2c417fd2706472f5b1f0efbe
sahilvishwakarma79222 <vsahil79222@gmail.com>
2026-02-09 18:53:08 +0530

    profiles in spring done

---
+++ microservice/QUIZ-CATEGORY/category-service/src/main/java/com/substring/quiz/category/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.substring.quiz.category.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/category/config")
+public class ConfigController {
+
+    @Value("${config.value}")
+    private String config;
+
+
+    @GetMapping
+    public ResponseEntity<?> getConfigValue(){
+        return new ResponseEntity<>(config, HttpStatus.OK);
+    }
+}
