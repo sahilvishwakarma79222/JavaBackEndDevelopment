afd2aa8557e97dcb1ad669c68b642e0518137d96
sahilvishwakarma79222 <vsahil79222@gmail.com>
2026-02-16 15:22:17 +0530

    auto refresh using actuator/refresh and spring cloud bus ... actuator/busrefresh is done

--- microservice/QUIZ-CATEGORY/quiz - resttemplate/src/main/java/com/substring/quiz/controller/ConfigController.java
+++ microservice/QUIZ-CATEGORY/quiz - resttemplate/src/main/java/com/substring/quiz/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.substring.quiz.controller;
 
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/quiz")
+@RefreshScope
 public class ConfigController {
 
     @Value("${config.value}")
