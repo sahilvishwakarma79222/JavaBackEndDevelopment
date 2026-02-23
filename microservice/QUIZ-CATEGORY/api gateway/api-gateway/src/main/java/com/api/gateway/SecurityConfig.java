b33db2441f996cc6212e42f08510566c926c6b0a
sahilvishwakarma79222 <vsahil79222@gmail.com>
2026-02-23 11:34:12 +0530

    role bases authorization is done.

--- microservice/QUIZ-CATEGORY/api gateway/api-gateway/src/main/java/com/api/gateway/SecurityConfig.java
+++ microservice/QUIZ-CATEGORY/api gateway/api-gateway/src/main/java/com/api/gateway/SecurityConfig.java
@@ -1,33 +1,52 @@
 package com.api.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+import org.springframework.core.convert.converter.Converter;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-
+    @Autowired
+    private RoleConverter roleConverter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
         http.cors(Customizer.withDefaults())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange-> exchange
-                                .anyExchange()
+                        .pathMatchers(HttpMethod.GET).hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.GET).hasAnyRole("ADMIN","USER")
+
+                        .pathMatchers(HttpMethod.POST).hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                        .anyExchange()
                                 .authenticated())
                 .oauth2ResourceServer
                 (
-                        config-> config.jwt(Customizer.withDefaults())
+                        config -> config.jwt(jwt -> jwt.jwtAuthenticationConverter(roleExtractor()))
                 );
-
         return http.build();
     }
 
-
+    private Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> roleExtractor() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(roleConverter);
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
 
 }
